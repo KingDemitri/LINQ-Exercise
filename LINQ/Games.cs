67c04009e0294dd6c587ef39c8dd975c0876fca8
Demitri Cameron <oblivion9794@gmail.com>
2023-01-19 07:44:15 -0500

    initial commmit

---
+++ LINQ/Games.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Games //: IEquatable<Games>
+    {
+
+        public string GameName { get; set; }
+
+        public string ReleaseOrder { get; set; }
+
+        public int ChronOrder { get; set; }
+        public override string ToString()
+        {
+            return "Name: " + GameName + "\n"+ "Release Order: " + ReleaseOrder + "\n" + 
+            "Chronological Release: " + ChronOrder + "\n";
+        }
+
+        //Referenced from https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-7.0
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj == null) return false;
+        //    Games objAsPart = obj as Games;
+        //    if (objAsPart == null) return false;
+        //    else return Equals(objAsPart);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return ReleaseOrder;
+        //}
+        //public bool Equals(Part other)
+        //{
+        //    if (other == null) return false;
+        //    return (this.PartId.Equals(other.PartId));
+        //}
+        // Should also override == and != operators.
+    }
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace LINQ
+﻿using System.IO;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Games> videogames = new List<Games>();//("Kingdom Hearts, Kingdom Hearts II, Kingdom Hearts II: Final Mix");
+
+            videogames.Add(new Games() { GameName = "Kingdom Hearts", ReleaseOrder = "1st", ChronOrder = 2 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts Final Mix", ReleaseOrder = "2nd", ChronOrder = 3 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts II", ReleaseOrder = "3rd", ChronOrder = 4 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts II: Final Mix", ReleaseOrder = "4th", ChronOrder = 5 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts: Chain of Memories", ReleaseOrder = "5th", ChronOrder = 3 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts X", ReleaseOrder = "6th", ChronOrder = 1 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts 0.2: A Fragmentary Passage", ReleaseOrder = "7th", ChronOrder = 2 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts 358/2 Days", ReleaseOrder = "8th", ChronOrder = 6 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts: RE:coded", ReleaseOrder = "9th", ChronOrder = 1 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts: Chain of Memories", ReleaseOrder = "10th", ChronOrder = 1 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts Dream Drop Distance", ReleaseOrder = "11th", ChronOrder = 10 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts 3", ReleaseOrder = "12th", ChronOrder = 13 });
+            videogames.Add(new Games() { GameName = "Kingdom Hearts: Birth By Sleep", ReleaseOrder = "12th", ChronOrder = 12 });
+
+            Console.WriteLine("Sorted by Release Order");
+            videogames.OrderBy(x => x.ChronOrder).ToList().ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Sorted by Release Order:");
+
+            videogames.OrderBy(x => x.ReleaseOrder).ToList().ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            IEnumerable<Games> sortedList = videogames.OrderByDescending(x => x.ReleaseOrder).ToList();
+
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);   
+            }
         }
     }
 }
+
+//Create a list of video game names...
+//    Order the list of games by length of the game name.
+//    Use the lambda expression in this exercise as well.
+
+//        use Method Syntax for this exercise
